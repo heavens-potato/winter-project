ee240b8193f8e75dc78c820b1540eb4e3d8541e3
heavens-potato <sswu2005@gmail.com>
2024-12-28 12:18:09 -0500

    Added feature boxes on home page

--- hydrohire/src/Landing.js
+++ hydrohire/src/Landing.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Button, Typography, Box } from '@mui/material';
+import { Button, Typography, Box, Card, CardContent } from '@mui/material';
 import Grid from '@mui/material/Grid2';
 import capybaraLogo from './assets/img/Capybara.png';
 
 function Landing() {
+  const features = [
+    {
+      title: 'Feature 1',
+      description: 'Description of Feature 1',
+    },
+    {
+      title: 'Feature 2',
+      description: 'Description of Feature 2',
+    },
+    {
+      title: 'Feature 3',
+      description: 'Description of Feature 3',
+    },
+  ];
+
   return (
+    <div>
     <Box sx={{ display: 'flex', height: '100vh', overflow: 'hidden' }}>
       {/* Left Half */}
       <Grid container item xs={6} justifyContent="center" alignItems="center">
@@ -56,6 +72,47 @@ function Landing() {
         </Box>
       </Grid>
     </Box>
+
+    {/* three features section */}
+    <Grid container spacing={3} sx={{
+          backgroundColor: (theme) => theme.palette.primary.dark,
+          display: 'flex',
+          flexDirection: { xs: 'column', lg: 'row' },
+          justifyContent: 'space-between', 
+          alignItems: 'center', 
+          padding: { xs: 5 , md: 15 },
+          gap: 5
+        }}>
+      {features.map((feature, index) => (
+        <Grid item xs={12} sm={4} key={index} sx={{
+          minWidth: { xs: 350, md: 600, lg: 350 },
+          borderRadius: '15px',
+        }}>
+          <Card>
+            <CardContent>
+              <Typography variant="h5" component="h2" sx={{
+                fontSize: 24,
+                color: (theme) => theme.palette.primary.main,
+                justifyContent: 'center',
+                textAlign: 'center',
+                fontWeight: 'bold',
+                padding: { xs: 2, md: 3 }
+                }}>
+                {feature.title}
+              </Typography>
+              <Typography variant="body2" sx={{
+                justifyContent: 'center',
+                textAlign: 'center',
+              }}>
+                {feature.description}
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+      ))}
+    </Grid>
+    </div>
+
   );
 }
 
