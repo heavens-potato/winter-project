9e018ae579713b54a5e2166954eb20b5eaf85623
cc13985 <claire13985@gmail.com>
2025-01-11 22:12:43 -0500

    Resized and centered charts

--- hydrohire/src/BarChartComponent.js
+++ hydrohire/src/BarChartComponent.js
@@ -3,13 +3,15 @@ import { BarChart, Bar, XAxis, YAxis, Tooltip, Legend } from 'recharts';
 
 const BarChartComponent = ({ data }) => {
   return (
-    <BarChart width={500} height={300} data={data}>
-      <XAxis dataKey="name" />
-      <YAxis />
-      <Tooltip />
-      <Legend />
-      <Bar dataKey="value" fill="#1976d2" />
-    </BarChart>
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <BarChart width={600} height={350} data={data}>
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Bar dataKey="value" fill="#1976d2" />
+      </BarChart>
+    </div>
   );
 };
 
--- hydrohire/src/PieChartComponent.js
+++ hydrohire/src/PieChartComponent.js
@@ -3,11 +3,13 @@ import { PieChart, Pie, Tooltip, Legend } from 'recharts';
 
 const PieChartComponent = ({ data }) => {
   return (
-    <PieChart width={500} height={300}>
-      <Pie data={data} dataKey="value" nameKey="name" fill="#1976d2" />
-      <Tooltip />
-      <Legend />
-    </PieChart>
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <PieChart width={500} height={350}>
+        <Pie data={data} dataKey="value" nameKey="name" fill="#1976d2" />
+        <Tooltip />
+        <Legend />
+      </PieChart>
+    </div>
   );
 };
 
