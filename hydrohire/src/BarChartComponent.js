e49e840e29f164adda6d60ea256b4a44a559f17b
cc13985 <claire13985@gmail.com>
2025-01-13 19:50:09 -0500

    Updated legend of bar charts

--- hydrohire/src/BarChartComponent.js
+++ hydrohire/src/BarChartComponent.js
@@ -11,13 +11,34 @@ const BarChartComponent = ({ data, barColors }) => {
   const chartHeight = isMobile ? 250 : 400;
   const xAxisFontSize = isMobile ? 10 : 15;
 
+  const renderLegend = () => {
+    return (
+      <ul style={{ listStyleType: 'none', padding: 0, margin: 0, textAlign: 'center' }}>
+        {data.map((entry, index) => (
+          <li key={`item-${index}`} style={{ display: 'inline-block', margin: '0 10px' }}>
+            <span
+              style={{
+                display: 'inline-block',
+                width: '10px',
+                height: '10px',
+                backgroundColor: barColors[index % barColors.length],
+                marginRight: '5px',
+              }}
+            ></span>
+            {entry.status}
+          </li>
+        ))}
+      </ul>
+    );
+  };
+
   return (
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', paddingTop: '25px', width: '100%', margin: 0 }}>
       <BarChart width={chartWidth} height={chartHeight} data={data}>
         <XAxis dataKey="status" style={{ fontSize: xAxisFontSize }} />
         <YAxis />
         <Tooltip />
-        <Legend />
+        <Legend content={renderLegend} />
         <Bar dataKey="count" name="Status" fill={barColors[5]}>
           {data.map((entry, index) => (
             <Cell key={`cell-${index}`} fill={barColors[index % barColors.length]} />
