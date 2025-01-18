6afa1a8e600b97c848df0d282fd624bf69e97103
Justin Glazer <jglazer365@gmail.com>
2025-01-17 20:24:02 -0500

    added updated information

--- hydrohire/src/About.js
+++ hydrohire/src/About.js
@@ -7,19 +7,16 @@ import { motion, AnimatePresence } from 'framer-motion';
 
 const features = [
   {
-    title: 'Feature 1',
-    description: 'Description of Feature 1',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+    title: 'Visual Insights & Dashboard',
+    description: 'Our visually engaging dashboard and insight overview keeps you on top of your job applications at a glance. With bar and pie charts and key metrics, you’ll have a snapshot of your progress throughout your journey. Track statuses, analyze trends, and stay motivated with these visualizations keeping you informed and in control.',
   },
   {
-    title: 'Feature 2',
-    description: 'Description of Feature 2',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+    title: 'Smart Application Tracker',
+    description: 'Effortlessly manage all your job applications in one convenient location. Our DataGrid structure makes it simple to add, view, edit, and delete entries. With powerful native and customized filtering options, you can quickly find the applications that matter most. You can also personalize your dashboard and display only the information that’s most relevant to you.',
   },
   {
-    title: 'Feature 3',
-    description: 'Description of Feature 3',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+    title: 'Personalized Workspace',
+    description: 'Make Job Capyture your own with customizable themes and account settings. Choose from nine vibrant color themes to match your style, and for a flexible and seamless workflow, easily and securely update your email, password, and profile settings whenever needed.',
   },
 ];
 
@@ -29,35 +26,37 @@ function About(){
     <>
       <Navbar/>
       <div> {/* Parent Div */}
-        <div style={{backgroundColor: theme.palette.primary.main}}> {/* Top Part */}
-          <div className = 'flex justify-center'>
-            <Typography variant="h4" sx={{ fontSize: 32}}>Who We Are</Typography>
-          </div>
-          <div className = 'flex justify-center mx-7'>
-            <p> Job Capyture simplifies and streamlines your job search process with an organized and customizable 
-              way to manage applications. We aim to solve the chaos of tracking applications across spreadsheets, 
-              emails, and scattered documents by offering a centralized, intuitive platform that visualizes progress 
-              and offers an organized method of viewing jobs. Designed for job seekers at all stages, 
-              Job Capyture is essential for maintaining clarity and confidence during the often-overwhelming job 
-              application journey.</p>
+        <div style={{backgroundColor: theme.palette.primary.dark}}> {/* Top Part */}
+          <div className = 'flex flex-col gap-6'>
+            <div className = 'flex justify-center'>
+              <Typography variant="h4" sx={{ fontSize: 32, color: theme.palette.primary.white}}>Who We Are</Typography>
+            </div>
+            <div className = 'flex justify-center mx-7'>
+              <p style={{color: theme.palette.primary.white}}> Job Capyture simplifies and streamlines your job search process with an organized and customizable 
+                way to manage applications. We aim to solve the chaos of tracking applications across spreadsheets, 
+                emails, and scattered documents by offering a centralized, intuitive platform that visualizes progress 
+                and offers an organized method of viewing jobs. Designed for job seekers at all stages, 
+                Job Capyture is essential for maintaining clarity and confidence during the often-overwhelming job 
+                application journey.</p>
+            </div>
           </div>
           {/* 3 Features */}
-            <motion.div
-              initial = {{ opacity: 0 }}
-              whileInView={{ opacity: 1 }} 
-              viewport={{ once: true }}
-              transition = {{ duration: 0.3 }}
-            >
+          <motion.div
+            initial = {{ opacity: 0 }}
+            whileInView={{ opacity: 1 }} 
+            viewport={{ once: true }}
+            transition = {{ duration: 0.3 }}
+          >
             <Grid container spacing={3} sx={{
-                  backgroundColor: (theme) => theme.palette.primary.main,
-                  display: 'flex',
-                  flexDirection: { xs: 'column', lg: 'row' },
-                  justifyContent: 'space-between', 
-                  alignItems: 'center', 
-                  padding: { xs: 5 , md: 15 },
-                  gap: 5
-                }}>
-              {features.map((feature, index) => (
+              backgroundColor: (theme) => theme.palette.primary.dark,
+              display: 'flex',
+              flexDirection: { xs: 'column', lg: 'row' },
+              justifyContent: 'space-between', 
+              alignItems: 'center', 
+              padding: { xs: 5 , md: 15 },
+              gap: 5
+              }}>
+                {features.map((feature, index) => (
                 <Grid item xs={12} sm={4} key={index} sx={{
                   minWidth: { xs: 350, md: 600, lg: 350 },
                   maxWidth: 350,
@@ -69,33 +68,33 @@ function About(){
                     viewport={{ once: true }}
                     transition = {{ duration: 0.3, delay: 0.3 }}
                   >
-                  <Card>
-                    <CardContent>
-                      <Typography variant="h5" component="h2" sx={{
-                        fontSize: 32,
-                        color: (theme) => theme.palette.primary.dark,
-                        justifyContent: 'center',
-                        textAlign: 'center',
-                        fontWeight: 'bold',
-                        padding: { xs: 2, md: 3 }
+                    <Card>
+                      <CardContent>
+                        <Typography variant="h5" component="h2" sx={{
+                          fontSize: 32,
+                          color: (theme) => theme.palette.primary.main,
+                          justifyContent: 'center',
+                          textAlign: 'center',
+                          fontWeight: 'bold',
+                          padding: { xs: 2, md: 3 }
+                          }}>
+                          {feature.title}
+                        </Typography>
+                        <Typography variant="body2" sx={{
+                          fontSize: 18,
+                          justifyContent: 'center',
+                          textAlign: 'center',
+                          wordWrap: 'break-word',
+                          color: (theme) => theme.palette.primary.dark,
                         }}>
-                        {feature.title}
-                      </Typography>
-                      <Typography variant="body2" sx={{
-                        fontSize: 18,
-                        justifyContent: 'center',
-                        textAlign: 'center',
-                        wordWrap: 'break-word',
-                        color: (theme) => theme.palette.primary.dark,
-                      }}>
-                        {feature.description}
-                      </Typography>
-                    </CardContent>
-                  </Card>
-                  </motion.div>           {/* 3 Features End */}
-                </Grid>
-              ))}
-            </Grid>
+                          {feature.description}
+                        </Typography>
+                      </CardContent>
+                    </Card>
+                    </motion.div>           {/* 3 Features End */}
+                  </Grid>
+                ))}
+              </Grid>
             </motion.div>
         </div> {/* Top End */}
         <div className = 'flex justify-center mt-10'>
